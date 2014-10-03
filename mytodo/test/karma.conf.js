ae238abe77accdbed85a7116e9e1dee0ab4b72e4
luohuazju <luohuazju@gmail.com>
2014-10-03 13:32:53 -0500

    no changes

--- mytodo/test/karma.conf.js
+++ mytodo/test/karma.conf.js
@@ -1,9 +1,11 @@
 // Karma configuration
 // http://karma-runner.github.io/0.12/config/configuration-file.html
-// Generated on 2014-06-06 using
-// generator-karma 0.8.2
+// Generated on 2014-08-29 using
+// generator-karma 0.8.3
 
 module.exports = function(config) {
+  'use strict';
+
   config.set({
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
@@ -16,14 +18,14 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'app/bower_components/angular/angular.js',
-      'app/bower_components/angular-mocks/angular-mocks.js',
-      'app/bower_components/angular-animate/angular-animate.js',
-      'app/bower_components/angular-cookies/angular-cookies.js',
-      'app/bower_components/angular-resource/angular-resource.js',
-      'app/bower_components/angular-route/angular-route.js',
-      'app/bower_components/angular-sanitize/angular-sanitize.js',
-      'app/bower_components/angular-touch/angular-touch.js',
+      'bower_components/angular/angular.js',
+      'bower_components/angular-mocks/angular-mocks.js',
+      'bower_components/angular-animate/angular-animate.js',
+      'bower_components/angular-cookies/angular-cookies.js',
+      'bower_components/angular-resource/angular-resource.js',
+      'bower_components/angular-route/angular-route.js',
+      'bower_components/angular-sanitize/angular-sanitize.js',
+      'bower_components/angular-touch/angular-touch.js',
       'app/scripts/**/*.js',
       'test/mock/**/*.js',
       'test/spec/**/*.js'
